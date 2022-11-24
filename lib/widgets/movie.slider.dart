ac3f32aeda27de484d900342ec448a760640d8a3
Daniel González Briseño <qbixmex@gmail.com>
2022-11-23 18:08:28 -0600

    09-finishing-movie-slider

--- lib/widgets/movie.slider.dart
+++ lib/widgets/movie.slider.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 
 class MovieSlider extends StatelessWidget {
   const MovieSlider({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
-      height: 250,
-      color: Colors.pink,
+      height: 270,
       child: Column(        
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
@@ -19,6 +17,7 @@ class MovieSlider extends StatelessWidget {
               style: Theme.of(context).textTheme.headline3,
             ),
           ),
+          const SizedBox(height: 10),
           Expanded(
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
@@ -33,17 +32,42 @@ class MovieSlider extends StatelessWidget {
 }
 
 class _MoviePost extends StatelessWidget {
-  const _MoviePost({super.key});
+
+  const _MoviePost();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.purple,
-      margin: const EdgeInsets.symmetric(
-        horizontal: 10,
-        vertical: 10,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(
+              context,
+              'details',
+              arguments: 'movie-instance',
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Movie Title: Return New Jedi and son of Luke the best',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
